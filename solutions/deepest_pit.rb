da3cfd60a4e6ff1366d40fa03c703c0a883cb4f7
Vitali Makarov <405112@gmail.com>
2014-11-09 15:24:42 +0300

    add deepest pit solution

---
+++ solutions/deepest_pit.rb
@@ -0,0 +1,28 @@
+class DeepestPit
+  class << self
+
+    def solution(a)
+      # puts a[0 .. 500].inspect
+      return -1 if a.size < 3
+
+      depth = 0
+      q = r = -1
+      p = 0
+
+      (1 ... a.size).each do |i|
+
+        q = i - 1 if q < 0 && a[i] >= a[i - 1]
+
+        if (q >= 0 && r < 0) && (a[i] <= a[i-1] || (i + 1) == a.size)
+          r = i - 1
+          depth = (a.size == 3 && (a[r] - a[q]) == 0) ? (a[p] - a[q]).abs : [depth, [a[p] - a[q], a[r] - a[q]].min].max
+          #puts "(#{p}, #{q}, #{r}) => (#{a[p]}, #{a[q]}, #{a[r]}) (#{a[p] - a[q]}, #{a[r] - a[q]}) => depth_curr: #{[a[p] - a[q], a[r] - a[q]].min}, depth: #{depth}"
+          p = i - 1
+          q = r = -1
+        end
+      end
+      depth = -1 if depth == 0
+      depth
+    end
+  end
+end
